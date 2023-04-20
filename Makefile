01cd1412fab9dff2878a403418420ceb654fcc9d
s4ch1n <sachin.mudaliyar@gmail.com>
2023-04-20 16:09:53 +0530

    SymbolTable implemented with cpp std::map

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ CFLAGS= -std=c++11 -Wall -Wextra -O0
 
 all: build
 
-build: JackAnalyzer.cc JackTokenizer.hh JackTokens.hh 
-	$(CC) $(CFLAGS) JackAnalyzer.cc -o JackAnalyzer
+build: JackCompiler.cc JackTokenizer.hh JackTokens.hh SymbolTable.hh
+	$(CC) $(CFLAGS) JackCompiler.cc -o JackCompiler
 
 submit: 
 	zip -R project10 Makefile *.cc *.hh lang.txt
---
+++ SymbolTable.hh
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+enum kind {
+  STATIC
+  , FIELD
+  , VAR
+  , ARG
+};
+
+
+
+class SymbolTable {
+  private:
+     uint64_t staticCount = 0;
+     uint64_t fieldCount = 0;
+     uint64_t _varCount = 0;
+     uint64_t argCount = 0;
+     std::map< std::string, 
+       std::tuple<std::string, enum::kind, uint64_t> > table; 
+
+  public:
+    SymbolTable() { };
+
+    ~SymbolTable() { }; 
+    
+    void define(std::string name, std::string type, enum::kind akind) {
+      uint64_t index;
+      switch (akind) {
+        case kind::STATIC: index = staticCount++; 
+          break;
+        case kind::FIELD: index = fieldCount++;
+          break;
+        case kind::VAR: index = _varCount++; 
+          break;
+        case kind::ARG: index = argCount++;
+          break;
+      }
+      table[name] = {type, akind, index};
+    }
+    
+    uint64_t varCount(enum::kind kind) {
+      switch (kind) {
+        case kind::STATIC: return staticCount; 
+        case kind::FIELD: return fieldCount; 
+        case kind::VAR: return _varCount; 
+        case kind::ARG: return argCount; 
+      }
+    } 
+
+    enum::kind kindOf(std::string name) {
+      return std::get<1>(table[name]);
+    } 
+
+    std::string typeOf(std::string name) {
+      return std::get<0>(table[name]);
+    }
+
+    uint64_t indexOf(std::string name) {
+     return std::get<2>(table[name]);
+    }
+
+    void reset() {
+      table.clear();
+      staticCount = 0; 
+      fieldCount = 0;
+      _varCount = 0;
+      argCount = 0;
+    }
+};
+
